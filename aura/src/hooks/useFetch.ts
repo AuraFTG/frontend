2313684bc0f7a050988e409352c1381f5ed268f9
adrian161103 <adriankeomaa@gmail.com>
2025-04-15 15:56:28 -0300

    primer fetch de prueba

---
+++ aura/src/hooks/useFetch.ts
@@ -0,0 +1,23 @@
+export interface FetchResult<T> {
+    status: "ok" | "error";
+    data: T | Error;
+}
+
+export const fetchWithOpts = async <T>(
+    url: string,
+    opts?: RequestInit
+): Promise<FetchResult<T>> => {
+    let result: FetchResult<T>;
+    try {
+        const response = await fetch(url, opts);
+        if (response.ok) {
+            const data = await response.json();
+            result = { status: "ok", data };
+        } else {
+            throw new Error(response.statusText);
+        }
+    } catch (error) {
+        result = { status: "error", data: error as Error };
+    }
+    return result;
+};
